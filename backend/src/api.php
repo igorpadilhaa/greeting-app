93e0eeecc648e4fed6303738ae95c3a6fb35b776
padilha <igor.padilha2004@gmail.com>
2023-06-07 18:57:59 -0300

    send json data from api

--- backend/src/api.php
+++ backend/src/api.php
@@ -1,5 +1,21 @@
 <?php
   header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
 
-  echo 'hello from api';
+  $greetings = [
+    [
+      'id' => 1,
+      'text' => 'Hello'       
+    ],
+    [
+      'id' => 2,
+      'text' => 'Howdy'       
+    ],
+    [
+      'id' => 3,
+      'text' => 'Hi'       
+    ]
+  ];
+
+  echo json_encode($greetings);
 ?>
